e6445c4e8161bb8f28fa5f96f640df14e10374e7
Mariana Naselli <nasellimariana@outlook.com>
2025-04-16 13:57:40 -0300

    refactor(category): improve service with validations, error handling and cleaner logic

--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -1,9 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
 import { Repository } from 'typeorm';
+import { NotFoundError } from 'rxjs';
+import e from 'express';
 
 @Injectable()
 export class CategoryService {
@@ -12,26 +14,60 @@ export class CategoryService {
     private categoryRepository: Repository<Category>,
   ) {}
 
-  create(createCategoryDto: CreateCategoryDto) {
+  async create(createCategoryDto: CreateCategoryDto) {
+    const existing = await
+    this.categoryRepository.findOne({
+      where:{
+        name:createCategoryDto.name,
+        isDeleted:false,
+      }
+    });
+    if(existing){
+      throw new BadRequestException('Ya existe una categoría con ese nombre');
+    }
+
     const category = this.categoryRepository.create(createCategoryDto);
     return this.categoryRepository.save(category);
   }
 
-  findAll() {
-    return this.categoryRepository.find({ where: { isDeleted: false } });
+  async findAll() {
+    const categories = await
+    this.categoryRepository.find({ where: { isDeleted: false }, order:{id:'ASC'}, });
+    if(!categories.length){
+      throw new NotFoundException('No hay categorías disponibles')
+    }
+    return categories;
+    
   }
 
-  findOne(id: number) {
-    return this.categoryRepository.findOne({ where: { id, isDeleted: false } });
+  async findOne(id: number) {
+    const category = await 
+    this.categoryRepository.findOne({ where: { id, isDeleted: false } });
+    if(!category) {
+      throw new NotFoundException(`Categoría con id ${id} no encontrada`)
+    }
+    return category;
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return this.categoryRepository.update(id, updateCategoryDto);
+//Actualizar la categoria si existe o si no está eliminada.
+  async update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    const category = await this.categoryRepository.findOne({
+      where: { id, isDeleted: false }, //buscamos la categoria por id(solo queremos categorias no eliminadas logicamente)
+    });
+    if (!category) {//si no encontramos una categoria valida.. lanzamos una excepción.
+      throw new NotFoundException(`Categoría con id ${id} no encontrada`); //error 404
+    }
+    const updateCategory = Object.assign(category, updateCategoryDto); //object.assign copia las propiedades del DTO al objeto original. Asi evitamos pisar todo el objeto o perder informacion importante como id o isDeleted
+    return this.categoryRepository.save(updateCategory);//Guardamos los cambios en la bd. Save puede tanto como crear o como actualizar (pero acá actualiza porque ya existe)
   }
 
   async remove(id: number) {
-    const category = await this.categoryRepository.findOne({ where: { id } });
-    if (!category) return null;
+    const category = await this.categoryRepository.findOne({
+      where: { id, isDeleted: false },
+    });
+    if (!category) {
+      throw new NotFoundException(`Categoría con id ${id} no encontrada`);
+    }
     category.isDeleted = true;
     return this.categoryRepository.save(category);
   }
